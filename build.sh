724f03096c17416c92e2e2a5e6832cff022349ac
suyuan <175338101@qq.com>
2024-06-02 11:09:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -100,8 +100,8 @@ fi
 if [ "$OMR_OPENWRT" = "default" ]; then
 	if [ "$OMR_KERNEL" = "5.4" ]; then
 		# Use OpenWrt 21.02 for 5.4 kernel
-		_get_repo "$OMR_TARGET/${OMR_KERNEL}/source" https://github.com/openwrt/openwrt "170d9e447df0f52882a8b7a61bf940b062b2cacc"
-		_get_repo feeds/${OMR_KERNEL}/packages https://github.com/openwrt/packages "b3a6bb839059546a52df00af3e1aa97dba75de22"
+		_get_repo "$OMR_TARGET/${OMR_KERNEL}/source" https://github.com/openwrt/openwrt "4a1d8ef55cbf247f06dae8e958eb8eb42f1882a5"
+		_get_repo feeds/${OMR_KERNEL}/packages https://github.com/openwrt/packages "1be343f776e7b035b20357fa7e8dcd14c6e4fb61"
 		_get_repo feeds/${OMR_KERNEL}/luci https://github.com/openwrt/luci "e4c46338b196e486a88b1a75b78e283708c82bc4"
 	else
 		_get_repo "$OMR_TARGET/${OMR_KERNEL}/source" https://github.com/openwrt/openwrt "895f38ca1efeb46f0cd3029c732e6156d4589eb0"
